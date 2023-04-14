1b6be032cd5123aa03432cc741b08961d2841668
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-04-14 17:10:18 +0200

    FEAT modify web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('comics.index');
+    return view('welcome');
 });
+
+Route::get('/', [ComicController::class, 'index']);
